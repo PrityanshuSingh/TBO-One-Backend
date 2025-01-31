654bd3e14c58ec733df536bb16f9474d82790347
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2025-02-01 05:16:11 +0530

    package fetch fixed

--- controllers/package/getAgentPackages.js
+++ controllers/package/getAgentPackages.js
@@ -2,8 +2,14 @@ const Package = require('../../models/Package');
 
 exports.getAgentPackages = async (req, res) => {
   try {
-    const packages = await Package.find({ agent: req.params.agentId });
-    res.status(200).json({ packages });
+    const packages = await Package.find({}).lean();
+
+    const transformedPackages = packages.map(pkg => ({
+      ...pkg,
+      id: pkg._id,
+    }));
+
+    res.status(200).json( transformedPackages );
   } catch (err) {
     res.status(400).json({ message: 'Error fetching packages', error: err });
   }
