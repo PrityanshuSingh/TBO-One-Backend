d3de99bf6ca368b49633a3bff1f54992ce155f3e
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2025-02-02 11:10:03 +0530

    all origins allowed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 mongoDB();
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGIN,
+    origin: process.env.ALLOWED_ORIGIN ||'*',
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
 }));
