aee93e216572d61f222b3082dd25a2b77e869508
prakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2025-02-03 02:01:41 +0530

    updated social media templates

--- models/Hotel.js
+++ models/Hotel.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 const hotelSchema = new mongoose.Schema({
   HotelRating: { type: String, required: true },
   CityCode: { type: String },
-  HotelCode: { type: String, required: true, unique: true },
+  HotelCode: { type: String, required: true },
   feature_embedding: { type: [Number], required: true },
   createdAt: { type: Date, default: Date.now }
 });
+
+// Compound Unique Index (HotelCode + CityCode)
+hotelSchema.index({ HotelCode: 1, CityCode: 1 }, { unique: true });
 
 module.exports = mongoose.model('hotels', hotelSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const router = express.Router();
 router.use("/auth",require('./auth'));
 router.use("/packages",require('./package'))
 router.use("/whatsapp",require('./whatsapp'))
+router.use("/ai",require('./ai'));
+router.use("/instagram",require("./instagram"))
 module.exports = router;
