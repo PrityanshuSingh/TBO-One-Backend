a61c42405fb4d39cbff97f96ae758204f659bebf
Prityanshu Singh <prityanshusingh2003@gmail.com>
2025-02-08 10:53:39 +0530

    Integrated Image Gen API

--- routes/ai.js
+++ routes/ai.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { getTemplate } = require('../controllers/campain/template/getTemplate');
 const generateCaption = require('../utils/genai/generateCaption');
+const generateImage = require('../utils/genai/generateImage');
 const generateWpDesc = require('../utils/genai/generateWpDesc');
+
 const router = express.Router();
 router.get("/templates", getTemplate);
 router.post("/caption", generateCaption);
+router.post("/image", generateImage);
 router.post("/wpDescription", generateWpDesc);
 module.exports = router;
